0936897a1cabfc4b18075ddf35476d257f40ddfa
Priyanka Naik <priyanka.naik@appfolio.com>
2021-01-05 12:44:51 -0800

    Fixed the URL that is being accessed

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
-class ErrorTest < ActionDispatch::IntegrationTest
+class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test 'should get index' do
-    get welcome_index_url
+    get root_url
     assert_response :success
   end
 end
