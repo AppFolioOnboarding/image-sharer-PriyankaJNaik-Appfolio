c6b0e30ec936faee83c72615030cd0948d544893
Priyanka Naik <priyanka.naik@appfolio.com>
2021-02-02 13:43:56 -0800

    Ability to delete images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,8 +2,8 @@ require 'image'
 
 class ImagesController < ApplicationController
   def index
-    if params[:tagName]
-      tag = params[:tagName]
+    if params[:tag_name]
+      tag = params[:tag_name]
       puts 'Searching images with tag... ' + tag
       @search_results = Image.tagged_with(tag).to_a.sort_by{ |a| a[:id] }.reverse
       @search_tag = tag
@@ -21,6 +21,14 @@ class ImagesController < ApplicationController
       flash[:saved_image] = "The image was saved!";
 
       @data_item = image;
+    end
+  end
+
+  def delete
+    if params[:image_id]
+      Image.delete(params[:image_id]);
+      flash[:deleted_image] = "The image with ID " + params[:image_id] + " was deleted!";
+      redirect_to({:action=>'index'});
     end
   end
 
